1f2ec7f416a36a640bb75628038a3df801ec447d
Max <ali.alipour4576@gmail.com>
2026-07-09 01:44:41 +0330

    Fav & Purchase page &userpanel & payment handle& contact ticket&

--- mini_digi/views.py
+++ mini_digi/views.py
@@ -3,6 +3,4 @@ from django_ratelimit.decorators import ratelimit
 
 @ratelimit(key='ip', rate='10/m', block=True)
 def ratelimited_error(request, exception=None):
-    return JsonResponse({'error': 'Too many requests'}, status=429)
-
-
+    return JsonResponse({'error': 'Too many requests. Please try again later.'}, status=429)
